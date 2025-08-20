ded2a38ad207063c372d47c7693aeb82c4dea15b
moxopills <moxopills@gmail.com>
2025-08-20 13:43:51 +0900

    add meeting service

--- app/dtos/create_meeting_response.py
+++ app/dtos/create_meeting_response.py
@@ -1,4 +1,3 @@
-
 from typing import Annotated
 
 from pydantic import BaseModel, Field
@@ -9,4 +8,4 @@ from app.dtos.frozen_config import FROZEN_CONFIG
 class CreateMeetingResponse(BaseModel):
     model_config = FROZEN_CONFIG
 
-    url_code: Annotated[str, Field(description="미팅 url 코드. unique 합니다.")]
+    url_code: Annotated[str, Field(description="meeting url code. unique do it.")]
