456f4971538fc40631af0bde223278841087b202
moxopills <moxopills@gmail.com>
2025-08-21 18:09:07 +0900

    Meeting service generation

--- app/api/v1/meeting_router.py
+++ app/api/v1/meeting_router.py
@@ -1,24 +1,21 @@
 from fastapi import APIRouter
 
 from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.tortoise_models.meeting import MeetingModel
 
-edgedb_router = APIRouter(prefix="/v1/edgedb/meeting", tags=["Meeting"], redirect_slashes=False)
-mysql_router = APIRouter(prefix="/v1/mysql/meeting", tags=["Meeting"], redirect_slashes=False)
-# 원래는 어떤 DB를 쓰는지 url에 적을 필요없음
-# 실전에선 DB이름 url에 넣지 말기
-
-
-@edgedb_router.post(
-    "",
-    description="meeting 을 생성합니다.",
+mysql_router = APIRouter(
+    prefix="/v1/mysql/meetings",
+    tags=["Meeting"],
+    redirect_slashes=False,
 )
-async def api_create_meeting_edgedb() -> CreateMeetingResponse:
-    return CreateMeetingResponse(url_code="abc")
 
 
 @mysql_router.post(
     "",
     description="meeting 을 생성합니다.",
+    response_model=CreateMeetingResponse,
 )
 async def api_create_meeting_mysql() -> CreateMeetingResponse:
-    return CreateMeetingResponse(url_code="abc")
+    meeting = await MeetingModel.create_meeting(url_code="abc")
+
+    return CreateMeetingResponse(url_code=meeting.url_code)
---
+++ app/tests/apis/v1/meeting_router.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+from app.dtos.create_meeting_response import CreateMeetingResponse
+from app.services.meeting_service_mysql import service_create_meeting_mysql
+
+edgedb_router = APIRouter(prefix="/v1/edgedb/meetings", tags=["Meeting"])
+mysql_router = APIRouter(prefix="/v1/mysql/meetings", tags=["Meeting"])
+
+
+@mysql_router.post("", description="meeting 을 생성합니다.")
+async def api_create_meeting_mysql() -> CreateMeetingResponse:
+    return CreateMeetingResponse(url_code=(await service_create_meeting_mysql()).url_code)
